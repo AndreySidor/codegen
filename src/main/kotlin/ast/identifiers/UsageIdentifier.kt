968d4322ae5c575a63933994b94ef01ae976fe8b
Lim <andrey.sidor.01@mail.ru>
2025-05-25 16:19:33 +0300

    UsageIdentifier теперь BaseElement и SpaceElement

--- src/main/kotlin/ast/identifiers/UsageIdentifier.kt
+++ src/main/kotlin/ast/identifiers/UsageIdentifier.kt
@@ -1,11 +1,13 @@
 package ast.identifiers
 
-import ast.elements.BodyElement
+import ast.BaseElement
 import ast.SingleLine
+import ast.elements.BodyElement
+import ast.elements.SpaceElement
 
 /**
  * Указатель на конкретное место в коде, может использоваться в задачах
  */
-object UsageIdentifier : SingleLine, BodyElement {
+object UsageIdentifier : SingleLine, BodyElement, BaseElement(), SpaceElement {
     override fun toString(): String = "/*!!??!!*/"
 }
