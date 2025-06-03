19b1d5fb7d80226a997cb7cdbd85eb6d3379879f
Lim <andrey.sidor.01@mail.ru>
2025-06-03 23:56:46 +0300

    В build gradle добавлена настройка для сборки jar файла

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,3 +30,23 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "MainKt"
+        )
+    }
+
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
