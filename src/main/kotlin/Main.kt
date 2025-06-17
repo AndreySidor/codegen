f163c83fe8cd846ef9646e7e60df09a105f912e1
Lim <andrey.sidor.01@mail.ru>
2025-06-17 09:11:32 +0300

    Реализована main функция генерации заданий из кода, а также печать файла навигации

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,10 @@ import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import patterns.Difficult
 import patterns.NavigatorData
+import patterns.classification.Classifier
+import patterns.code.CodeEngine
 import patterns.template.TemplateEngine
 import templates.Templates
 import java.io.File
@@ -118,8 +119,16 @@ fun runTemplateEngine(args: Array<String>) {
                 tmpFile.writeText(formattedCode)
 
                 // Генерация задачи под каждый префикс
-                prefixes.forEach { prefix ->
+                for (prefix in prefixes) {
                     try {
+                        // Расстановка знаний в задаче
+                        val knowledge = Classifier.classify(prefix, code)
+
+                        // Если задание не содержит знаний, то не генерируем его
+                        if (knowledge.isEmpty()) {
+                            continue
+                        }
+
                         // Имя файлов ttl и json
                         val outputFileName = randomString(16)
 
@@ -134,8 +143,15 @@ fun runTemplateEngine(args: Array<String>) {
                             val navigatorFile = outputPath.resolve("navigator_${outputFileName}.json").toFile().apply {
                                 createNewFile()
                             }
-                            val navigatorData = NavigatorData.create(outputPath, outputParseData, outputFileName)
-                            navigatorFile.writeText(Json.encodeToString(navigatorData))
+                            val navigatorData = NavigatorData.create(
+                                outputPath,
+                                outputParseData,
+                                outputFileName,
+                                prefix,
+                                formattedCode,
+                                knowledge
+                            )
+                            navigatorFile.writeText(json.encodeToString(navigatorData))
                             success++
                         } else {
                             errorParse++
@@ -155,6 +171,180 @@ fun runTemplateEngine(args: Array<String>) {
         println("Success: $success")
         println("Generate error: $errorGenerate")
         println("Parse error: $errorParse")
+    } catch (e : Exception) {
+        e.printError()
+    }
+}
+
+/**
+ * Генерация из кода
+ * @param args аргументы
+ */
+fun runCodeEngine(args: Array<String>) {
+    try {
+        // Проверить наличие необходимых модулей
+        val jarDir = JarPath.get() ?: throw IllegalStateException("Not found path to CodeEngine.jar dir")
+        val jarDirPath = Paths.get(jarDir).toAbsolutePath()
+
+        // Путь к exe файлу парсера в ttl и json
+        val pathToTTLAndJsonParser = jarDirPath.toFile().walkTopDown().find {
+            it.isFile && it.nameWithoutExtension == "ParseToTTLAndJson" && it.extension == "exe"
+        }?.toPath()?.toAbsolutePath() ?: throw IllegalStateException("Not found module ParseToTTLAndJson.exe in CodeEngine.jar dir and childes")
+
+        // Путь к парсеру кода в шаблон
+        val pathToTemplateParser = jarDirPath.toFile().walkTopDown().find {
+            it.isFile && it.nameWithoutExtension == "ParseToTemplate" && it.extension == "exe"
+        }?.toPath()?.toAbsolutePath() ?: throw IllegalStateException("Not found module ParseToTemplate.exe in CodeEngine.jar dir and childes")
+
+        // Загрузка данных для автоподстановок
+        Templates.preload()
+
+        // Парсинг аргументов
+        val argParser = ArgParser("CodeEngine")
+
+        val input by argParser.argument(
+            type = ArgType.String,
+            fullName = "input",
+            description = "Path to file of template or directory with codes .cpp"
+        )
+        val output by argParser.option(
+            type = ArgType.String,
+            fullName = "output",
+            shortName = "o",
+            description = "Path output directory"
+        ).default(Paths.get("output").absolutePathString())
+
+        argParser.parse(args)
+
+        // Создаем выходную директорию, если ее нет
+        val outputPath = try {
+            val tmpOutput = Paths.get(output)
+            if (!Files.exists(tmpOutput)) {
+                Files.createDirectories(tmpOutput)
+            }
+            tmpOutput.toAbsolutePath()
+        } catch (e: Exception) {
+            throw IllegalStateException("Ошибка при создании директории для выходных данных: ${e.message}")
+        }
+
+        // Путь к выходной директории для результатов парсера ttl и json
+        val outputParseData = outputPath.resolve("data").apply {
+            Files.createDirectories(this)
+        }.toAbsolutePath()
+
+        // Временный файл для сохранения шаблона
+        val tmpTemplateFile = Files.createTempFile("code_engine_template_", ".txt").toFile().apply {
+            deleteOnExit()
+        }
+
+        // Директория расположения временного файла
+        val tmpFileDir = Paths.get(tmpTemplateFile.parentFile.absolutePath)
+
+        // Временный файл для сохранения кода и парсинга через ParseToTTLAndJson.exe
+        val tmpCodeFile = Files.createTempFile("code_engine_code_", ".cpp").toFile().apply {
+            deleteOnExit()
+        }
+
+        // Кол-во успешно сгенерированных задач
+        var success = 0
+
+        // Кол-во ошибок при генерации кода и префиксов
+        var errorGenerate = 0
+
+        // Кол-во ошибок при парсинге в ttl и json
+        var errorParse = 0
+
+        // Кол-во ошибок при парсинге кода в шаблон
+        var errorParseToTemplate = 0
+
+        // Получаем все файлы cpp по входному пути
+        val codes = getFiles(input, listOf("cpp"))
+
+        codes.forEach { file ->
+            try {
+                // Прасинг cpp файла в шаблон
+                val (result, message) = runExe(
+                    exePath = pathToTemplateParser.absolutePathString(),
+                    args = listOf(file.absolutePath, tmpFileDir.absolutePathString(), tmpTemplateFile.nameWithoutExtension)
+                )
+                if (result) {
+                    try {
+                        // Чтение сгенерированного шаблона из tmp файла
+                        val template = readFiles(tmpTemplateFile.absolutePath, listOf("txt")).firstOrNull()
+                            ?: throw IllegalStateException("Can not read tmp_template file by path: ${tmpTemplateFile.absolutePath}}")
+
+                        // Генерация заданий
+                        val generatedValues = CodeEngine.generate(template)
+
+                        generatedValues.forEach { (prefixes, code) ->
+                            // Форматирование кода
+                            val formattedCode = formatCodeWithClangFormat(code.toString()) ?: throw IllegalStateException("Error clang-format")
+
+                            // Запись код во временный файл
+                            tmpCodeFile.writeText(formattedCode)
+
+                            // Генерация задачи под каждый префикс
+                            for (prefix in prefixes) {
+                                try {
+                                    // Расстановка знаний в задаче
+                                    val knowledge = Classifier.classify(prefix, code)
+
+                                    // Если задание не содержит знаний, то не генерируем его
+                                    if (knowledge.isEmpty()) {
+                                        continue
+                                    }
+
+                                    // Имя файлов ttl и json
+                                    val outputFileName = randomString(16)
+
+                                    // Парсинг ttl и json
+                                    val (result, message) = runExe(
+                                        exePath = pathToTTLAndJsonParser.absolutePathString(),
+                                        args = listOf(prefix.toString(), tmpCodeFile.absolutePath, outputParseData.absolutePathString(), outputFileName)
+                                    )
+
+                                    // Если парсинг прошел успешно, то создаем файл навигации
+                                    if (result) {
+                                        val navigatorFile = outputPath.resolve("navigator_${outputFileName}.json").toFile().apply {
+                                            createNewFile()
+                                        }
+                                        val navigatorData = NavigatorData.create(
+                                            outputPath,
+                                            outputParseData,
+                                            outputFileName,
+                                            prefix,
+                                            formattedCode,
+                                            knowledge
+                                        )
+                                        navigatorFile.writeText(json.encodeToString(navigatorData))
+                                        success++
+                                    } else {
+                                        errorParse++
+                                        throw RuntimeException(message)
+                                    }
+                                } catch (e : Exception) {
+                                    e.printError()
+                                }
+                            }
+                        }
+                    } catch (e : Exception) {
+                        errorGenerate++
+                        e.printError()
+                    }
+                } else {
+                    errorParseToTemplate++
+                    throw RuntimeException(message)
+                }
+            } catch (e : Exception) {
+                e.printError()
+            }
+        }
+
+        // Печать результатов
+        println("Success: $success")
+        println("Generate error: $errorGenerate")
+        println("Parse to ttl and json error: $errorParse")
+        println("Parse to template error: $errorParseToTemplate")
     } catch (e : Exception) {
         e.printError()
     }
--- src/main/kotlin/patterns/NavigatorData.kt
+++ src/main/kotlin/patterns/NavigatorData.kt
@@ -1,17 +1,25 @@
 package patterns
 
 import kotlinx.serialization.Serializable
+import patterns.classification.ScopeKnowledge
+import patterns.prefix.PrefixGenerator
 import java.nio.file.Path
 
 /**
  * Хранит данные для навигации по файлам задачи ttl и json, которые создал парсер
  * @param ttl относительный путь к ttl файлу
  * @param json относительный путь к json файлу
+ * @param prefix префикс задачи
+ * @param code код задачи
+ * @param knowledge список знаний и ошибок в задаче
  */
 @Serializable
 data class NavigatorData(
+    val knowledge : List<ScopeKnowledge>,
     val ttl : String,
-    val json : String
+    val json : String,
+    val prefix : String,
+    val code : String
 ) {
     companion object {
         /**
@@ -19,10 +27,19 @@ data class NavigatorData(
          * @param outputDir путь к выходной директории
          * @param name имя выходного файла без расширения
          */
-        private fun createInstance(outputDir: Path, name: String): NavigatorData {
+        private fun createInstance(
+            outputDir: Path,
+            name: String,
+            prefix : PrefixGenerator.Prefix,
+            code : String,
+            knowledge: List<ScopeKnowledge>
+        ): NavigatorData {
             return NavigatorData(
                 ttl = outputDir.resolve("$name.ttl").normalize().toString(),
-                json = outputDir.resolve("$name.json").normalize().toString()
+                json = outputDir.resolve("$name.json").normalize().toString(),
+                prefix = prefix.toString(),
+                code = code,
+                knowledge = knowledge
             )
         }
 
@@ -31,11 +48,18 @@ data class NavigatorData(
          * @param target целевая директория расположения сгенерированных фалов парсером
          * @param name имя файла без расширения
          */
-        fun create(base: Path, target: Path, name: String): NavigatorData {
+        fun create(
+            base: Path,
+            target: Path,
+            name: String,
+            prefix: PrefixGenerator.Prefix,
+            code: String,
+            knowledge: List<ScopeKnowledge>
+        ): NavigatorData {
 
             // Вычисление относительного пути от файла навигации к сгенерированным файлам
             val relativeDir = base.toAbsolutePath().relativize(target.toAbsolutePath())
-            return createInstance(relativeDir, name)
+            return createInstance(relativeDir, name, prefix, code, knowledge)
         }
     }
 }
