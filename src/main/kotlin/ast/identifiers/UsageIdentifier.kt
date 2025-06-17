40762a8dee37fe2c35e1cf38dada19d4591e198e
Lim <andrey.sidor.01@mail.ru>
2025-06-17 09:02:15 +0300

    Реализована функция установки UsageIdentifier внутрь функции

--- src/main/kotlin/ast/identifiers/UsageIdentifier.kt
+++ src/main/kotlin/ast/identifiers/UsageIdentifier.kt
@@ -3,10 +3,14 @@ package ast.identifiers
 import ast.BaseElement
 import ast.Serializable
 import ast.SingleLine
+import ast.elements.Body
 import ast.elements.BodyElement
+import ast.elements.Function
 import ast.elements.SpaceElement
+import patterns.findAll
 import patterns.serializers.ElementSerializer
 import patterns.serializers.IdentifierSerializer
+import selectMiddleElement
 
 /**
  * Указатель на конкретное место в коде, может использоваться в задачах
@@ -19,4 +23,23 @@ class UsageIdentifier : SingleLine, BodyElement, BaseElement(), SpaceElement, Se
     override fun clone(): BaseElement = UsageIdentifier()
 
     override fun toString(): String = "/*..??..*/"
+
+    fun setIn(function: Function) : Function {
+        if (function.body == null) {
+            return function.apply {
+                body = Body(
+                    elements = mutableListOf(this@UsageIdentifier)
+                )
+                updateRelations()
+            }
+        }
+
+        val bodies = function.body?.findAll { it is Body } as? List<Body>
+
+        val body = bodies?.selectMiddleElement() ?: function.body!!
+
+        body.elements.add(body.elements.count() / 2, this@UsageIdentifier)
+
+        return function.apply { updateRelations() }
+    }
 }
